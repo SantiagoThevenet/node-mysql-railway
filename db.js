2957cbc1af74e24d69f2d70e4b2e211530fd3515
santiago <santithevenetvalles@gmail.com>
2023-01-26 09:31:52 +0100

    Variables de entornos cambiadas

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { createPool } = require('mysql2/promise')
-
+const {host,user,password,port,database} = require('./db/config')
 const pool = createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DB
+    host,
+    user,
+    password,
+    port,
+    database
 })
 
 module.exports = pool
